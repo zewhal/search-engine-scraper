4c0b209f24732e9d2bff9ba30649917f597ac5df
Michael John Angelo Belciña <mjabelcina.dev@gmail.com>
2025-12-05 09:40:53 +0800

    feat(api): expose search engines, client, and SERP types in root exports

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+export { duckduckgo } from "./src/search-engine/duckduckgo";
+export { ecosia } from "./src/search-engine/ecosia";
+
+export { createCuimpClient, createPatchrightClient } from "./src/client"
+export type {
+  SERPQueryParams,
+  SERPResponse,
+  SERPResultItem
+} from "./src/types/serp";
